815d28c4f66a67d37a054cd0e08a4ab0b28ce854
Santiago Pintos <spintos@outlook.es>
2024-07-09 16:19:44 -0300

    Add enpoint to create a new payment preference

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var port = process.env.PORT || 3000
-
 const mercadopago = require('mercadopago');
 
 const client = new mercadopago.MercadoPagoConfig({
@@ -12,69 +11,14 @@ const client = new mercadopago.MercadoPagoConfig({
     
     }
 });
-
 const preference = new mercadopago.Preference({client});
-preference.create({
-    body: {
-        payment_methods: {
-            excluded_payment_methods: [
-                {
-                    id: "visa"
-                }
-            ],
-            excluded_payment_types: [
-                {
-                    id: "ticket"
-                }
-            ],
-            installments: 6
-        },
-        items: [
-            {
-                id: "",
-                title: "",
-                description: "Dispositivo móvil de Tienda e-commerce",
-                picture_url: "",
-                quantity: 1,
-                unit_price: 100,
-
-            }
-        ],
-        payer: {
-            name: "Lalo",
-            surname: "Landa",
-            email: "test_user_36961754@testuser.com",
-            phone: {
-                area_code: "+598",
-                number: 21996,
-            },
-            identification: {
-                type: "CI",
-                //auto generated from: https://picandocodigo.github.io/ci_js/
-                number: "91533399",
-            },
-            address: {
-                street_name: "Street street",
-                street_number: 1602,
-                zip_code: "90000",
-            }
-        },
-        back_urls: {
-            "success": "https://www.success.com",
-            "failure": "http://www.failure.com",
-            "pending": "http://www.pending.com"
-        },
-        auto_return: "approved",
-        notification_url: "https://webhook.site/",
-        external_reference: process.env.EMAIL,
-    }
-})
-.then(console.log)
-.catch(console.error);
 
 
 var app = express();
- 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
@@ -105,6 +49,65 @@ app.get('/checkout', function (req, res) {
 });
 app.post('/notifications', function (req, res) {
     res.status(200).send('OK');
+});
+
+app.post('/new_preference', function (req, res) {
+    preference.create({
+        body: {
+            payment_methods: {
+                excluded_payment_methods: [
+                    {
+                        id: "visa"
+                    }
+                ],
+                excluded_payment_types: [
+                    {
+                        id: "ticket"
+                    }
+                ],
+                installments: 6
+            },
+            items: [
+                {
+                    id: Number(1),
+                    title: req.body.title,
+                    description: "Dispositivo móvil de Tienda e-commerce",
+                    picture_url: req.body.img,
+                    quantity: Number(req.body.unit),
+                    unit_price: Number(req.body.price),
+                }
+            ],
+            payer: {
+                name: "Lalo",
+                surname: "Landa",
+                email: "test_user_36961754@testuser.com",
+                phone: {
+                    area_code: "+598",
+                    number: 21996,
+                },
+                identification: {
+                    type: "CI",
+                    //auto generated from: https://picandocodigo.github.io/ci_js/
+                    number: "91533399",
+                },
+                address: {
+                    street_name: "Street street",
+                    street_number: 1602,
+                    zip_code: "90000",
+                }
+            },
+            back_urls: {
+                "success": "https://www.success.com",
+                "failure": "http://www.failure.com",
+                "pending": "http://www.pending.com"
+            },
+            auto_return: "approved",
+            notification_url: "https://webhook.site/",
+            external_reference: process.env.EMAIL,
+        }
+    })
+    .then(console.log)
+    .catch(console.error);
 });
 
 app.listen(port);
